457349234c0c8f4596cec74e5bb4daf7b80d7511
Andrei Chirita <adchir95@gmail.com>
2022-10-07 19:28:00 +0000

    committing with code changes

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let vampireCount = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      vampireCount++;
+    }
+
+    return vampireCount;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,6 +48,28 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVamp = this;
+
+    if (currentVamp.numberOfVampiresFromOriginal === 0) {
+      return currentVamp
+    } else if (vampire.numberOfVampiresFromOriginal === 0) {
+      return vampire;
+    }
+
+    while (currentVamp.numberOfVampiresFromOriginal !== vampire.numberOfVampiresFromOriginal) {
+      if (currentVamp.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+        vampire = vampire.creator;
+      } else if (currentVamp.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal){
+        currentVamp = currentVamp.creator;
+      }
+    }
+
+    while (currentVamp.name !== vampire.name) {
+      currentVamp = currentVamp.creator;
+      vampire = vampire.creator;
+    }
+
+    return currentVamp;
 
   }
 }
